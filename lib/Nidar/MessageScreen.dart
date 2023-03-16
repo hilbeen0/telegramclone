05dc6a3e591a23b8a4f038a973c6ff63292f0254
Alndbarware <alndduhoki32@gmail.com>
2023-03-16 22:59:10 +0300

    create the routes and add all screen in the routes

---
+++ lib/Nidar/MessageScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class message extends StatelessWidget {
+  const message({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/hlbin/chatScreen.dart
+++ lib/hlbin/chatScreen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class chat extends StatelessWidget {
+  const chat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
