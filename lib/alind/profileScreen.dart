c8cb4688533b4159af1d51e3f42ca2385351f757
Alndbarware <alndduhoki32@gmail.com>
2023-03-16 22:25:12 +0300

    create stateles widget name profile

--- lib/alind/profileScreen.dart
+++ lib/alind/profileScreen.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
+class profile extends StatelessWidget {
+  const profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
