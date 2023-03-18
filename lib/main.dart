0d8530d0d5747082fd58d3b8fc43ba6482f45628
Alndbarware <alndduhoki32@gmail.com>
2023-03-18 17:02:12 +0300

    create the routes inside main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:telegramclone/Eryvan/newmassage.dart';
+import 'package:telegramclone/Nidar/MessageScreen.dart';
+import 'package:telegramclone/alind/profileScreen.dart';
+import 'package:telegramclone/hlbin/chatScreen.dart';
+import 'package:telegramclone/rayan/rayan.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    initialRoute: '/chat',
+    routes: {
+      '/profile': (context) => profile(),
+      '/chat': (context) => chat(),
+      '/newmessage': (context) => newmessage(),
+      '/message': (context) => message(),
+      '/acount': (context) => acount()
+    },
+  ));
 }
