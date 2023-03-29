1ac01aa711d346a243fa3b443b5c9e2fde3f58b0
hilbeen <hilbeen00@gmail.com>
2023-03-29 20:52:44 +0300

    data

---
+++ lib/hlbin/card.dart
@@ -0,0 +1,17 @@
+class DataConfig {
+  String name;
+  String mass;
+  String date;
+  String image;
+  bool num;
+  int num1;
+
+  DataConfig({
+    required this.name,
+    required this.image,
+    required this.mass,
+    required this.date,
+    required this.num,
+    required this.num1,
+  });
+}
