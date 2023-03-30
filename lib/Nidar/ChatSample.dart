10570ce95406878468349e1a6507b4ce5f714788
Nidar Hussein <nidarduski8@gmail.com>
2023-03-30 22:42:30 +0300

    install custom clipper

--- lib/Nidar/ChatSample.dart
+++ lib/Nidar/ChatSample.dart
@@ -1,4 +1,4 @@
-//import 'package:custom_clippers/custom_clippers.dart';
+import 'package:custom_clippers/custom_clippers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +11,7 @@ class ChatSample extends StatelessWidget{
       children: [
         Padding(padding: EdgeInsets.only(right: 80),
         child: ClipPath(
-          //clipper: UpperNipMessageClipper(MessageType.recieve),
+          clipper: UpperNipMessageClipper(MessageType.receive),
           child: Container(
            padding: 
             EdgeInsets.only(top: 10,bottom: 10,left: 25,right: 10,),
@@ -28,7 +28,7 @@ class ChatSample extends StatelessWidget{
           alignment: Alignment.centerRight,
           margin: EdgeInsets.only(top: 20,left: 80,bottom: 15),
           child: ClipPath(
-           // clipper: UpperNipMessageClipper(MessageType.send),
+          clipper: UpperNipMessageClipper(MessageType.send),
            child: Container(
               padding: 
              EdgeInsets.only(top: 10,bottom: 10,left: 10,right: 20,),
