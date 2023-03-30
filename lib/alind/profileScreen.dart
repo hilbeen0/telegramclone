c2b8dd945ed7fa5be03e4fda43fd87611e4512cc
Alndbarware <alndduhoki32@gmail.com>
2023-03-30 17:38:42 +0300

    container

--- lib/alind/profileScreen.dart
+++ lib/alind/profileScreen.dart
@@ -8,7 +8,9 @@ class profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 12,
       height: 12,
+      color: Colors.amber,
     );
   }
 }
