1746cfe553f77dedfc41884e76ab73bf56921b1d
Alndbarware <alndduhoki32@gmail.com>
2023-03-30 21:31:08 +0300

    return listview

--- lib/alind/profileScreen.dart
+++ lib/alind/profileScreen.dart
@@ -7,10 +7,8 @@ class profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 12,
-      height: 12,
-      color: Colors.amber,
+    return ListView(
+      children: [],
     );
   }
 }
