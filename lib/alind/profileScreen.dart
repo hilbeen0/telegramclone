1dddb7bb297536cf9a43d7d27930cc4cdc96b39b
Alndbarware <alndduhoki32@gmail.com>
2023-03-30 21:51:19 +0300

    inside listview create corrent acount picture

--- lib/alind/profileScreen.dart
+++ lib/alind/profileScreen.dart
@@ -12,24 +12,29 @@ class profile extends StatelessWidget {
         GestureDetector(
           onTap: () {},
           child: UserAccountsDrawerHeader(
-              accountName: Text(
-                "alnd barware",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
-              ),
-              accountEmail: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "0751 797 9339",
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
-                  ),
-                  Icon(
-                    Icons.keyboard_arrow_down_outlined,
-                    size: 25,
-                    color: Colors.white,
-                  )
-                ],
-              )),
+            accountName: Text(
+              "alnd barware",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+            ),
+            accountEmail: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "0751 797 9339",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w800),
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down_outlined,
+                  size: 25,
+                  color: Colors.white,
+                )
+              ],
+            ),
+            currentAccountPicture: CircleAvatar(
+              child: FlutterLogo(size: 42),
+              backgroundColor: Colors.amber,
+            ),
+          ),
         )
       ],
     );
