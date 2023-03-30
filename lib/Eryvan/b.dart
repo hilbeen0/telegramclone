aa6f74c900d3e39584d29ae95d1a257993414fa0
eryvanmahmod <cseryvanascii@gmail.com>
2023-03-30 11:55:45 +0300

    a

--- lib/Eryvan/b.dart
+++ lib/Eryvan/b.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class conectcard extends StatelessWidget {
+class newgr extends StatelessWidget {
   //const conectcard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Container(
-        child: Expanded(
-          child: Container(
-            width: 1100,
-            height: 75,
-            color: Colors.white,
+    return Stack(
+      children: [
+        Container(
+          child: Expanded(
+            child: Container(
+              width: 1100,
+              height: 75,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-      Positioned(
+        Positioned(
           top: 50,
           left: 55,
           bottom: 0.5,
@@ -28,22 +29,24 @@ class conectcard extends StatelessWidget {
               color: Colors.white,
               child: Positioned(
                 child: Center(
-                  child: Text("New Group",
+                  child: Text("New Channel",
                       style: TextStyle(
                         fontSize: 10,
                         color: Colors.black,
                         fontWeight: FontWeight.w700,
                       )),
                 ),
-              ))),
-      Positioned(
-          top: 10,
-          height: 100,
-          width: 100,
-          child: Icon(
-            Icons.supervisor_account,
-            color: Colors.grey,
-          )),
-    ]);
+              )),
+        ),
+        Positioned(
+            top: 10,
+            height: 100,
+            width: 100,
+            child: Icon(
+              Icons.supervisor_account,
+              color: Colors.grey,
+            )),
+      ],
+    );
   }
 }
