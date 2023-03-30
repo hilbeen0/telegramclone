762cec2ae20305a679f5fd3e74dfc5f21593801f
Nidar Hussein <nidarduski8@gmail.com>
2023-03-30 17:26:13 +0300

    ChatPage

---
+++ lib/Nidar/ChatSample.dart
@@ -0,0 +1,55 @@
+//import 'package:custom_clippers/custom_clippers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChatSample extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: EdgeInsets.only(right: 80),
+        child: ClipPath(
+          clipper: UpperNipMessageClipper(MessageType.recieve),
+          child: Container(
+            padding: 
+            EdgeInsets.only(top: 10,bottom: 10,left: 25,right: 10,),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 255, 254, 254),
+            ),
+            child: Text("Hi Nidar ,How are you?.   2:30pm",
+        style: TextStyle(fontSize: 17,color: Colors.black),
+            ),
+          ),
+        ),
+        ),
+        Container(
+          alignment: Alignment.centerRight,
+          margin: EdgeInsets.only(top: 20,left: 80,bottom: 15),
+          child: ClipPath(
+            clipper: UpperNipMessageClipper(MessageType.send),
+            child: Container(
+              padding: 
+              EdgeInsets.only(top: 10,bottom: 10,left: 10,right: 20,),
+              decoration: BoxDecoration(
+                color: Color(0xFFE4FDCA)
+              ),
+              child: Text("Hellow ,I am  fine and well,and thanks for asking .               4:30pm",
+              style: TextStyle(fontSize: 17,color: Colors.black),
+              ),
+              
+            ),
+          ),
+        )
+      ],
+    );
+  }
+  
+  UpperNipMessageClipper(recieve) {}
+}
+
+class MessageType {
+  static var recieve;
+  
+  static var send;
+}
--- lib/Nidar/MessageScreen.dart
+++ lib/Nidar/MessageScreen.dart
@@ -1,11 +1,28 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+//main.dart
+import 'package:flutter/material.dart';
+import 'package:telegramclone/Nidar/chat_page.dart';
+void main() {
 
-class message extends StatelessWidget {
-  const message({super.key});
+ runApp(MyApp()) ;
+}
+class MyApp extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+         theme: ThemeData(
+        scaffoldBackgroundColor: Color.fromARGB(255, 255, 255, 255),
+        appBarTheme: AppBarTheme(
+          color: Color.fromARGB(133, 43, 120, 122),
+        ),
+          bottomSheetTheme: BottomSheetThemeData(
+       backgroundColor: Colors.black.withOpacity(0),
+       ),
+      ),
+       routes: {
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+        "/":((context) => ChatPage()),
+      },  
+      );
+    }
 }
