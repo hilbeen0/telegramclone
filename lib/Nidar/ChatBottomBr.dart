a7854db3b504e6eb5e9f6e076a2e883c19538704
Nidar Hussein <nidarduski8@gmail.com>
2023-03-31 15:49:16 +0300

    change the width chat bottom

--- lib/Nidar/ChatBottomBr.dart
+++ lib/Nidar/ChatBottomBr.dart
@@ -22,7 +22,7 @@ class ChatBottomBr extends StatelessWidget{
                 size: 30,
                 ),
                 SizedBox(width: 10,),
-                Container(width: 257,
+                Container(width: 253,
                 child: TextFormField(
                   style: TextStyle(
                     fontSize: 19,
