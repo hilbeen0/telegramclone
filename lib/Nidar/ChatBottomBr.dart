08d872bdbd18aad9143b3efdebc3756d365b6c2d
Nidar Hussein <nidarduski8@gmail.com>
2023-03-31 16:57:28 +0300

    225

--- lib/Nidar/ChatBottomBr.dart
+++ lib/Nidar/ChatBottomBr.dart
@@ -22,7 +22,7 @@ class ChatBottomBr extends StatelessWidget{
                 size: 30,
                 ),
                 SizedBox(width: 10,),
-                Container(width: 229,
+                Container(width: 225,
                 child: TextFormField(
                   style: TextStyle(
                     fontSize: 19,
