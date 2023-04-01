8814f5f5f13d77868f64d1d4e9f4f17392923406
Nidar Hussein <nidarduski8@gmail.com>
2023-04-02 00:07:47 +0300

    iuiou

--- lib/Nidar/MessageScreen.dart
+++ lib/Nidar/MessageScreen.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
       return MaterialApp(
         debugShowCheckedModeBanner: false,
          theme: ThemeData(
-        scaffoldBackgroundColor: Color.fromARGB(221, 7, 172, 243),
+        scaffoldBackgroundColor: Color.fromARGB(255, 7, 212, 243),
         appBarTheme: AppBarTheme(
           color: Color.fromARGB(156, 7, 145, 230),
         ),
